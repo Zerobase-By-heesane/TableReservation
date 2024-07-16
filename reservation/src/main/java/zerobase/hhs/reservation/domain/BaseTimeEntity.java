95eb15363ae1288002658f05fe9fd4e6ae3e34d0
heesane <tees3359@gmail.com>
2024-07-17 01:25:45 +0900

    BaseTime Entity

---
+++ reservation/src/main/java/zerobase/hhs/reservation/domain/BaseTimeEntity.java
@@ -0,0 +1,30 @@
+package zerobase.hhs.reservation.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+@ToString
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    public void delete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+}
