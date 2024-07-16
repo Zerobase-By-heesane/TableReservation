beb49bb13cc29a913b4c3923d23420294f7e115c
heesane <tees3359@gmail.com>
2024-07-17 01:25:02 +0900

    User Service Interface

---
+++ reservation/src/main/java/zerobase/hhs/reservation/service/UserService.java
@@ -0,0 +1,16 @@
+package zerobase.hhs.reservation.service;
+
+import zerobase.hhs.reservation.Type.UserRole;
+import zerobase.hhs.reservation.dto.request.UserLoginRequest;
+import zerobase.hhs.reservation.dto.request.UserRegisterRequest;
+import zerobase.hhs.reservation.dto.response.UserLoginResponse;
+import zerobase.hhs.reservation.dto.response.UserRegisterResponse;
+
+public interface UserService {
+
+    UserRegisterResponse register(UserRegisterRequest request);
+
+    UserLoginResponse login(UserLoginRequest request);
+
+    UserRole getUserRole(Long userId);
+}
