6c47ecb48fa9533c9a5bf6834b7b2d51322b0407
heesane <tees3359@gmail.com>
2024-07-18 11:34:17 +0900

    Auth Controller

---
+++ reservation/src/main/java/zerobase/hhs/reservation/controller/AuthController.java
@@ -0,0 +1,36 @@
+package zerobase.hhs.reservation.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import zerobase.hhs.reservation.dto.request.UserLoginRequest;
+import zerobase.hhs.reservation.dto.request.UserRegisterRequest;
+import zerobase.hhs.reservation.dto.response.UserLoginResponse;
+import zerobase.hhs.reservation.dto.response.UserRegisterResponse;
+import zerobase.hhs.reservation.service.UserService;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserRegisterResponse> register(@RequestBody @Valid UserRegisterRequest request) {
+        log.info("request: {}", request);
+        return ResponseEntity.ok(userService.register(request));
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserLoginResponse> login(@RequestBody @Valid UserLoginRequest request){
+        return ResponseEntity.ok(userService.login(request));
+    }
+}
