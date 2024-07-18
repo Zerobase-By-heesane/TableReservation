db0976626933f5e6d44d0fdb3694f097ec12dca7
heesane <tees3359@gmail.com>
2024-07-18 11:35:15 +0900

    Login Filter

---
+++ reservation/src/main/java/zerobase/hhs/reservation/jwt/LoginFilter.java
@@ -0,0 +1,47 @@
+package zerobase.hhs.reservation.jwt;
+
+import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LoginFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = this.validateToken(request);
+        log.info("request: {}",request);
+
+        // 토큰 유효성 겅증
+        if(token != null && jwtProvider.validateToken(token)) {
+            Authentication authentication = jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, response);
+        }else{
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        }
+    }
+
+    private String validateToken(HttpServletRequest request){
+        String token = request.getHeader("Authorization");
+
+        if(StringUtils.isNotEmpty(token) && token.startsWith("Bearer ")){
+            return token.split(" ")[1];
+        }else return null;
+    }
+}
