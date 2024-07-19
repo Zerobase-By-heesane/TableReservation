c0fca94c4587bde147038870f793e7e900e45af8
heesane <tees3359@gmail.com>
2024-07-20 02:12:42 +0900

    Reservation Entity

--- reservation/src/main/java/zerobase/hhs/reservation/domain/Reservation.java
+++ reservation/src/main/java/zerobase/hhs/reservation/domain/Reservation.java
@@ -1,4 +1,19 @@
 package zerobase.hhs.reservation.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
