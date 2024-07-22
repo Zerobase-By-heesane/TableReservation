4ac1ec71b2b42460f200c10888b866bfe59a1b14
heesane <tees3359@gmail.com>
2024-07-22 18:08:04 +0900

    Reservation Service Interface

---
+++ reservation/src/main/java/zerobase/hhs/reservation/service/ReservationService.java
@@ -0,0 +1,34 @@
+package zerobase.hhs.reservation.service;
+
+import zerobase.hhs.reservation.dto.request.reserve.*;
+import zerobase.hhs.reservation.dto.response.reserve.*;
+
+public interface ReservationService {
+
+    // 유저
+    // 내 예약 목록
+    ReserveListResponse getMyReservationList(ReserveListRequest request);
+
+    // 예약
+    ReserveResponse reserve(ReserveRegisterRequest request);
+
+    // 취소
+    ReserveCancelResponse cancel(ReserveCancelRequest request);
+
+    // 체크인
+    ReserveCheckInResponse checkIn(ReserveCheckInRequest request);
+
+    // 체크아웃
+    ReserveCheckOutResponse checkOut(ReserveCheckOutRequest request);
+
+    // 파트너
+    // 예약 목록
+    ReserveListResponse getReservationList(ReserveListRequest request);
+
+    // 예약 승인
+    ReserveConfirmResponse approveReservation(ReserveConfirmRequest request);
+
+    // 예약 거부
+    ReserveDenyResponse rejectReservation(ReserveDenyRequest request);
+
+}
