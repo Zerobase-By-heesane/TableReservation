83f5d171f04a684ba8ce696e9b158285e1a18b87
heesane <tees3359@gmail.com>
2024-07-22 18:07:54 +0900

    Reservation 컬럼 추가

--- reservation/src/main/java/zerobase/hhs/reservation/domain/Reservation.java
+++ reservation/src/main/java/zerobase/hhs/reservation/domain/Reservation.java
@@ -1,13 +1,43 @@
 package zerobase.hhs.reservation.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import zerobase.hhs.reservation.type.ApprovedType;
+import zerobase.hhs.reservation.type.ReserveType;
 
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@ToString
 @Entity
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "reservation_id")
     private Long id;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @Column(name = "reserve_time", nullable = false)
+    private LocalDateTime reserveTime;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @Column(name = "check_in_time", nullable = true)
+    private LocalDateTime checkInTime;
+
+    @Column(name = "people", nullable = false)
+    private int people;
+
+    @Column(name = "check_status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ReserveType checkStatus;
+
+    @Column(name = "approved_status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ApprovedType approvedStatus;
 
     @ManyToOne
     @JoinColumn(name = "store_id")
@@ -16,4 +46,16 @@ public class Reservation {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    public void updateCheckInTime(LocalDateTime time){
+        this.checkInTime = time;
+    }
+
+    public void updateApprovedStatus(ApprovedType approvedStatus){
+        this.approvedStatus = approvedStatus;
+    }
+
+    public void updateCheckStatus(ReserveType checkStatus){
+        this.checkStatus = checkStatus;
+    }
 }
