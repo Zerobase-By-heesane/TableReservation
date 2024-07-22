0e06cbe85dc748506f71dab203532209cb24f615
heesane <tees3359@gmail.com>
2024-07-23 02:29:44 +0900

    입력 타입 변환

--- reservation/src/main/java/zerobase/hhs/reservation/controller/PartnerReservationController.java
+++ reservation/src/main/java/zerobase/hhs/reservation/controller/PartnerReservationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import zerobase.hhs.reservation.dto.request.reserve.ReserveConfirmRequest;
 import zerobase.hhs.reservation.dto.request.reserve.ReserveDenyRequest;
-import zerobase.hhs.reservation.dto.request.reserve.ReserveListRequest;
+import zerobase.hhs.reservation.dto.request.reserve.ReserveRequest;
 import zerobase.hhs.reservation.dto.response.reserve.ReserveConfirmResponse;
 import zerobase.hhs.reservation.dto.response.reserve.ReserveDenyResponse;
 import zerobase.hhs.reservation.dto.response.reserve.ReserveListResponse;
@@ -21,7 +21,7 @@ public class PartnerReservationController {
     private final ReservationService reservationService;
 
     @GetMapping("/list")
-    public ResponseEntity<ReserveListResponse> getReservationList(ReserveListRequest request){
+    public ResponseEntity<ReserveListResponse> getReservationList(ReserveRequest request){
         return ResponseEntity.ok(reservationService.getReservationList(request));
     }
 
