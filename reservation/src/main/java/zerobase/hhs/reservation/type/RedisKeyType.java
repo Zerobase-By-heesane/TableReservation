6db886a9e458e1229ff6a896588291a1c54c6283
heesane <tees3359@gmail.com>
2024-07-24 21:22:06 +0900

    Redis Key Type

---
+++ reservation/src/main/java/zerobase/hhs/reservation/type/RedisKeyType.java
@@ -0,0 +1,7 @@
+package zerobase.hhs.reservation.type;
+
+public class RedisKeyType {
+    public static final String RESERVATION = "reservationCache";
+    public static final String REFRESH = "refreshCache";
+    public static final String STORES = "storesCache";
+}
