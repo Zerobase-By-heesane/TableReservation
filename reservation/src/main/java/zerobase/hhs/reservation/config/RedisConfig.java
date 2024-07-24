687c2d22635bd6c8cfc0dac39b3ea8584b0d034e
heesane <tees3359@gmail.com>
2024-07-24 21:21:45 +0900

    Redis Config

---
+++ reservation/src/main/java/zerobase/hhs/reservation/config/RedisConfig.java
@@ -0,0 +1,96 @@
+package zerobase.hhs.reservation.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+import static java.time.Duration.ofMillis;
+
+@EnableCaching
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String HOSTNAME;
+
+    @Value("${spring.data.redis.port}")
+    private Integer PORT;
+
+    @Value("${spring.data.redis.database}")
+    private Integer DATABASE;
+
+    @Value("${spring.data.redis.password}")
+    private String PASSWORD;
+
+    @Value("${spring.data.redis.timeout}")
+    private Long TIMEOUT;
+
+    @Value("${jwt.refresh-expiration}")
+    private Integer REFRESH_EXPIRATION;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(HOSTNAME);
+        config.setPort(PORT);
+        config.setDatabase(DATABASE);
+        config.setPassword(PASSWORD);
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(ofMillis(TIMEOUT))
+                .build();
+
+        return new LettuceConnectionFactory(config, clientConfig);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(){
+        // 사람의 수를 확인하기위한
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        // Key : Value 형식으로 사용할 경우 시리얼라이저
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        // Hash를 사용할 경우 시리얼라이저
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+        // 모든 경우
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration(60))
+                .withCacheConfiguration("refresh", redisCacheConfiguration(REFRESH_EXPIRATION))
+                .withCacheConfiguration("reservation", redisCacheConfiguration(180))
+                .build();
+    }
+
+    private  RedisCacheConfiguration redisCacheConfiguration(int seconds) {
+        return RedisCacheConfiguration
+                .defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofSeconds(seconds));
+    }
+}
