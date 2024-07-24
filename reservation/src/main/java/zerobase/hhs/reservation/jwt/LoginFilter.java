b0d041289e0458eeab030b1a0116f785e29e33e7
heesane <tees3359@gmail.com>
2024-07-25 07:02:42 +0900

    Redis + Refresh Token 교환 로직 추가

--- reservation/src/main/java/zerobase/hhs/reservation/jwt/LoginFilter.java
+++ reservation/src/main/java/zerobase/hhs/reservation/jwt/LoginFilter.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import zerobase.hhs.reservation.domain.User;
+import zerobase.hhs.reservation.service.RefreshService;
 import zerobase.hhs.reservation.service.UserService;
 
 import java.io.IOException;
@@ -24,36 +25,54 @@ public class LoginFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
     private final JwtTokenUtil jwtTokenUtil;
     private final UserService userService;
+    private final RefreshService refreshService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // Authorization 헤더에 토큰이 잘 들어가 있는지 확인
+        // 없는 경우, return;
         String token = validateToken(request);
         if (token == null) {
             filterChain.doFilter(request, response);
             return;
         }
 
+        // Extract Token
+        // If Token is Invalid, return 401
         Claims claims = jwtProvider.validateToken(token);
         if (claims == null) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
 
-        Date expiration = claims.getExpiration();
+        // If Access Token is Expired, validate Refresh Token
+        if (claims.getExpiration().before(new Date())) {
 
-        // 만료된 경우
-        if (expiration.before(new Date())) {
-            User user = userService.getUser(Long.parseLong(claims.getSubject()));
-            String userRefreshToken = user.getRefresh();
+            // Get User
+            Long userId = Long.parseLong(claims.getSubject());
+            User user = userService.getUser(userId);
 
+            // Validate Refresh Token Through Cache
+            String userRefreshToken = refreshService.cacheRefreshToken(userId);
+            if (userRefreshToken == null) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            // Extract User Information
             Claims userRefreshClaims = jwtProvider.validateToken(userRefreshToken);
             Date userRefreshExpiration = userRefreshClaims.getExpiration();
 
+            // If Refresh Token is Expired, return status code 401
             if (userRefreshExpiration.before(new Date())) {
                 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                 return;
-            } else {
+            }
+            // Else Generate New Access Token
+            else {
                 String newAccessToken = jwtTokenUtil.generateToken(user).accessToken();
+                // Set New Access Token to Response Header
                 response.setHeader("Authorization", "Bearer " + newAccessToken);
                 token = newAccessToken;
             }
