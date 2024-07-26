5125fd337daf582e1eb555c3c3b88160b7217274
heesane <tees3359@gmail.com>
2024-07-26 15:36:45 +0900

    Review Detail

---
+++ reservation/src/main/java/zerobase/hhs/reservation/domain/model/ReviewDetail.java
@@ -0,0 +1,11 @@
+package zerobase.hhs.reservation.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReviewDetail {
+    private int fulfillment;
+    private String content;
+}
