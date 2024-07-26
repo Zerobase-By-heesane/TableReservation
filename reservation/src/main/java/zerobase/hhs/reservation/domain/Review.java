ccf9e63379529a3f7152e566c3b574af21f11505
heesane <tees3359@gmail.com>
2024-07-26 15:11:38 +0900

    Review Persistence Entity

--- reservation/src/main/java/zerobase/hhs/reservation/domain/Review.java
+++ reservation/src/main/java/zerobase/hhs/reservation/domain/Review.java
@@ -1,4 +1,37 @@
 package zerobase.hhs.reservation.domain;
 
-public class Review {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="review")
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class Review extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    @Column(name="fulfillment", nullable = false)
+    private Integer fulfillment;
+
+    @Column(name="content",nullable = false)
+    private String content;
+
+    // 1 대 1 매핑
+    @OneToOne
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+
+    public void updateReview(Integer fulfillment, String content){
+        this.fulfillment = fulfillment;
+        this.content = content;
+    }
 }
